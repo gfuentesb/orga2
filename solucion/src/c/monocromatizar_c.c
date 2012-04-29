27abc8f6c6c6eef19374db9d03217f036466250c
Marco Vanotti <mvanotti@dc.uba.ar>
2012-04-28 23:16:22 -0300

    Code style fixes to monocromatizar_c
    It stills needs testing, plus, mono needs to be saturated

--- solucion/src/c/monocromatizar_c.c
+++ solucion/src/c/monocromatizar_c.c
@@ -2,17 +2,23 @@
 #define ALFA 0.21
 #define BETA 0.71
 #define GAMA 0.07
-
-
+#define PIXEL_SIZE 4
 
 void monocromatizar_c (unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size) {
-	int ih;
-    int idh;
-    for (ih = idh = 0; ih < h*src_row_size; ih += src_row_size, idh += dst_row_size) {
-        int iw;
-        for (iw = 0; iw < w*4; iw = iw+4) {
-            monocromatizar(&src[ih+iw], &dst[idh+iw]);
-        }
+
+    unsigned int srcrow = 0;
+    unsigned int dstrow = 0;
+    int r = 0;
+
+    while (r < h) {
+
+        for (int c = 0; c < w; c += 1) {
+            monocromatizar(src[srcrow + (c * PIXEL_SIZE)], dst[dstrow + (c * PIXEL_SIZE)]);
+    	}
+    	
+        srcrow += src_row_size;
+        dstrow += dst_row_size;
+        r += 1;
     }
 }
 
