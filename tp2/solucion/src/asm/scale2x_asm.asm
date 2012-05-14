197043d90c2c262e8220eda79a1c126d0475853c
Marco Vanotti <mvanotti@dc.uba.ar>
2012-05-14 00:49:06 -0300

    hecho scale2x

--- tp2/solucion/src/asm/scale2x_asm.asm
+++ tp2/solucion/src/asm/scale2x_asm.asm
@@ -24,7 +24,6 @@ global scale2x_asm
 %define magic_mask_2 0xD8 ; 11011000 ABCD -> ACBD
 
 scale2x_asm:
-;COMPLETAR
     push rbp
     mov rbp, rsp
 
@@ -44,8 +43,16 @@ scale2x_asm:
 
     mov rcx, h
     shr rcx, 1
-
-    shl w, 1
+    mov r8, w
+    shr w, 2
+    shl w, 3
+    shl r8, 62
+    shr r8, 62
+    
+    mov r9, w
+    mov rdi, r8
+    shl rdi, 1
+    
 
 .copio_fila:
 
@@ -53,6 +60,75 @@ scale2x_asm:
     xor rbx, rbx
 
     .copio_columna:
+            movdqu xmm0, [src + rax]
+            add src, src_rs
+            movdqu xmm1, [src + rax]
+            add src, src_rs
+            movdqu xmm2, [src + rax]
+            sub src, src_rs
+            sub src, src_rs
+
+            ;shifteamos para comparar D con F
+            movdqa xmm7, xmm1
+            psrldq xmm7, 8
+            pcmpeqd xmm7, xmm1
+
+            ;comparamos B con H
+            movdqa xmm6, xmm0
+            pcmpeqd xmm6, xmm2
+
+            ;Hacemos D == F || B == H
+            psrldq xmm6, 4
+            por xmm7, xmm6
+            pshufd xmm7, xmm7, magic_mask
+
+            movdqa xmm6, xmm1
+            movdqa xmm3, xmm1
+
+            pshufd xmm0, xmm0, magic_mask_1 ;ABCD -> BBCC
+            pshufd xmm2, xmm2, magic_mask_1 ;GHIJ -> HHII
+            pshufd xmm3, xmm3, magic_mask_2 ;DEFK -> DFEK
+            pshufd xmm6, xmm6, magic_mask_1 ;DEFK -> EEFF Si sale mal queremos dejar este valor.
+
+            movdqa xmm4, xmm3
+            pcmpeqd xmm4, xmm0
+            movdqa xmm5, xmm3
+            pcmpeqd xmm5, xmm2
+
+            movdqa xmm2, xmm7
+            pandn xmm7, xmm4
+            pandn xmm2, xmm5
+            
+            movdqa xmm0, xmm3
+            movdqa xmm1, xmm3
+            
+            pand xmm0, xmm7
+            pandn xmm7, xmm6
+            paddusb xmm0, xmm7
+
+            pand xmm1, xmm2
+            pandn xmm2, xmm6
+            paddusb xmm1, xmm2
+
+            movdqu [dst + rbx], xmm0
+            add dst, dst_rs
+            movdqu [dst + rbx], xmm1
+
+            add rbx, 16
+            sub dst, dst_rs
+
+            add rax, 8
+            cmp rax, r9
+
+            jne .copio_columna
+            
+        ; process remaining pixels
+        cmp r8, 3
+        jne .end_line
+        
+        sub rax, 4
+        sub rbx, 8
+
         movdqu xmm0, [src + rax]
         add src, src_rs
         movdqu xmm1, [src + rax]
@@ -106,23 +182,17 @@ scale2x_asm:
         movdqu [dst + rbx], xmm0
         add dst, dst_rs
         movdqu [dst + rbx], xmm1
-
-        add rbx, 16
         sub dst, dst_rs
-
-        add rax, 8
-        cmp rax, w
-
-        jne .copio_columna
-
-    add src, src_rs
-    add dst, dst_rs
-    add dst, dst_rs
+        
+.end_line
+        add src, src_rs
+        add dst, dst_rs
+        add dst, dst_rs
 
 
-    sub rcx, 1
-    cmp rcx, 0
-    jne .copio_fila
+        sub rcx, 1
+        cmp rcx, 0
+        jne .copio_fila
 
     pop r12
     pop rbx
