d2f980f087081c981d5d1fd640e9e8e59f562494
Marco Vanotti <mvanotti@dc.uba.ar>
2012-06-03 20:10:43 -0300

    GDT ej1 a

--- tp3/entregable/gdt.c
+++ tp3/entregable/gdt.c
@@ -10,24 +10,56 @@
 
 gdt_entry gdt[GDT_COUNT] = {
 	/* Descriptor nulo*/
-	(gdt_entry){ 
-		(unsigned short) 0x0000, 
-		(unsigned short) 0x0000,
-		(unsigned char) 0x00, 
-		(unsigned char) 0x0, 
-		(unsigned char) 0, 
-		(unsigned char) 0, 
-		(unsigned char) 0, 
-		(unsigned char) 0x0,
-		(unsigned char) 0,  
-		(unsigned char) 0,  
-		(unsigned char) 0,  
-		(unsigned char) 0, 
-		(unsigned char) 0x00 
-	}
-
-	/* COMPLETAR */
-
+    (gdt_entry){(unsigned int) 0x00000000, (unsigned int) 0x00000000 },
+    /* Data Segment */
+    [4] = {
+        .limit_0_15 = 0xFFFF,
+        .base_0_15 = 0,
+        .base_23_16 = 0,
+        .type = 0x2, // Writable
+        .s = 1,
+        .dpl = 0,
+        .p = 1,
+        .limit_16_19 = 0xF,
+        .avl = 0,
+        .l = 0,
+        .db = 1,
+        .g = 1,
+        .base_31_24 = 0
+    },
+    /* Code Segment */
+    [6] = {
+        .limit_0_15 = 0xFFFF,
+        .base_0_15 = 0,
+        .base_23_16 = 0,
+        .type = 0xA, // Executable, Readable
+        .s = 1,
+        .dpl = 0,
+        .p = 1,
+        .limit_16_19 = 0xF,
+        .avl = 0,
+        .l = 0,
+        .db = 1,
+        .g = 1,
+        .base_31_24 = 0
+    },
+    /* Segmento de video */
+    [2] =
+    {
+        .limit_0_15 = (80 * 25 * 2) - 1,
+        .base_0_15 = 0x8000,
+        .base_23_16 = 0xB,
+        .type = 0x2, // Non-executable, Readable, Writable
+        .s = 1,
+        .dpl = 0,
+        .p = 1,
+        .limit_16_19 = 0x0,
+        .avl = 0,
+        .l = 0,
+        .db = 1,
+        .g = 0,
+        .base_31_24 = 0
+    }
 };
 
 gdt_descriptor GDT_DESC = {sizeof(gdt)-1, (unsigned int)&gdt};
