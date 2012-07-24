1450d141e1f701a7e8d1503cbe24655e6a46d6da
Marco Vanotti <mvanotti@dc.uba.ar>
2012-07-23 23:52:02 -0300

    Comments.

--- tp3/entregable/kernel.asm
+++ tp3/entregable/kernel.asm
@@ -54,7 +54,6 @@ extern inicializar_idt
 
 extern resetear_pic
 extern habilitar_pic
-extern print_the_name
 
 extern chequear_A20
 extern clean_screen
@@ -108,20 +107,14 @@ modo_protegido:
         call clean_screen
         call inicializar_idt
         lidt [IDT_DESC]
-        xchg bx, bx
+
         call init_mmu
 
-        mov eax, 0x21000
+        mov eax, 0x21000 ;Cargamos la pos del directorio de páginas en CR3
         mov cr3, eax
         mov eax, cr0
-        or eax, 0x80000000
-        mov ecx, $
+        or eax, 0x80000000 ; Habilitar la paginación
         mov cr0, eax
-        mov eax, 1
-        add eax, 2
-
-        xchg bx, bx
-        call print_the_name
 
 		jmp $
 
